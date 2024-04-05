dc1287f1e9d27924111bb8f1d1631a58aa25b62d
Joshua Gutierrez <joshgm15@gmail.com>
2024-04-05 15:21:44 -0500

    router setup complete

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import Browse from './Browse';
 import './App.css';
 import Cart from './Cart';
+import Confirmation from './Confirmation'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Browse />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
       </BrowserRouter>
   )
