438883fae60be35276027c53ce9fbbd3e8975c2f
Tristan <ricemaking69@gmail.com>
2024-04-05 15:57:51 -0500

    Chore: updated the UI design for the confirmation

--- assignment2/src/Confirmation.js
+++ assignment2/src/Confirmation.js
@@ -4,16 +4,16 @@ function Confirmation() {
 
     return (
         <div className="container">
-            <div className="row justify-content-center my-5">
+            <div className="row justify-content-center my-5 ">
                 <div className="col-md-5">
                     <div className="card">
-                        <div className="card-body">
+                        <div className="card-body text-center">
                             <h1 className="card-title">Summary:</h1>
-                            <p className="card-text">Tristan Nono</p>
-                            <p className="card-text">trist020703@yahoo.com</p>
-                            <p className="card-text">1234</p>
-                            <p className="card-text">123 North Street</p>
-                            <p className="card-text">Chicago, Illinois, 60006</p>
+                            <p className="card-text border-bottom">Name: Tristan Nono</p>
+                            <p className="card-text border-bottom">Email: trist020703@yahoo.com</p>
+                            <p className="card-text border-bottom">Credit Card: 1234</p>
+                            <p className="card-text border-bottom">Address: 123 North Street</p>
+                            <p className="card-text border-bottom">Chicago, Illinois, 60006</p>
                             <button className="btn btn-primary">Submit</button>
                         </div>
                     </div>
