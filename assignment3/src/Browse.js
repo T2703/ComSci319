6f98d3eba344325585747cf4b0d4690165c39e01
Tristan <ricemaking69@gmail.com>
2024-04-18 13:44:36 -0500

    Worked a bit on the browse and installed some dependencies

---
+++ assignment3/src/Browse.js
@@ -0,0 +1,11 @@
+
+function Browse() {
+   return <div>
+      <div className="col">
+         <input type="search" className="form-control search-input w-50" placeholder="Search"></input>
+         <button className="btn btn-primary" >Checkout</button>
+      </div>
+   </div>
+}
+
+export default Browse;
