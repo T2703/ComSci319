362dbc8e74d79157724d7c1c4db35a8ea0ab0028
Tristan <ricemaking69@gmail.com>
2024-04-18 19:37:02 -0500

    UI has been updated for browse

--- assignment3/src/Browse.js
+++ assignment3/src/Browse.js
@@ -26,30 +26,39 @@ function Browse() {
         <div className="container">
           <div className="row">
             <div className="col">
-              <input
-                type="search"
-                className="form-control search-input w-50"
-                placeholder="Search"
-              />
-              <button className="btn btn-primary">Checkout</button>
+              <div className="input-group mb-5">
+                <input
+                  type="search"
+                  className="form-control search-input offset-md-10"
+                  placeholder="Search"
+                  style={{ maxWidth: "200px", marginTop: "10px" }}
+                />
+              </div>
+              <div className="col d-flex justify-content-end align-items-center">
+                <button className="btn btn-primary">Checkout</button>
+              </div>
             </div>
           </div>
         </div>
         <div className="container mt-3">
-          <div className="row">
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
             {data.map((item) => (
-              <div key={item.id} className="col-md-4 mb-3">
-                <div className="card">
+              <div key={item.id} className="col">
+                <div className="card h-100">
                   <img
                     src={item.image}
-                    className="card-img-top"
+                    className="card-img-top img-fluid"
                     alt={item.title}
+                    style={{ height: "200px", objectFit: "contain" }}
                   />
-                  <div className="card-body">
+                  <div className="card-body d-flex flex-column">
                     <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text">{item.description}</p>
                     <p className="card-text">{item.rating.rate}</p>
-                    <button className="btn btn-primary">+</button>
-                    <button className="btn btn-primary">-</button>
+                    <div className="mt-auto">
+                      <button className="btn btn-primary">+</button>
+                      <button className="btn btn-primary">-</button>
+                    </div>
                   </div>
                 </div>
               </div>
