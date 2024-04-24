bb456ca0ca04263c1e267523b850a691f3f61e32
Tristan <ricemaking69@gmail.com>
2024-04-24 15:04:53 -0500

    Delete method added into frontend

--- assignment3/frontend/src/Delete.js
+++ assignment3/frontend/src/Delete.js
@@ -1,20 +1,54 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Delete() {
+    const navigate = useNavigate();
+    const [productId, setProductId] = useState("");
+    const handleDelete = async (productId) => {
+        try {
+            await fetch(`http://localhost:5000/products/${productId}`, {
+                method: 'DELETE',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(
+                    { "id": productId}
+                )
+            });
+            alert("Product deleted!");
+            navigate('/');
+            console.log("Deleted", productId);
+            
+        } catch (error) {
+            console.error('ERROR:', error);
+        }
+    };
+
+    const handleChange = (event) => {
+        setProductId(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleDelete(productId);
+    };
+
+
     return (
         <div className="container mt-4">
             <div className="row justify-content-center">
                 <div className="col-md-6">
                     <h2 className="text-center">Delete Product</h2>
                     <h2 className="text-center">Enter in the product ID</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
-                            <label htmlFor="title" className="form-label">ID:</label>
+                            <label htmlFor="productId" className="form-label">ID:</label>
                             <input
                                 type="text"
                                 className="form-control"
-                                id="title"
+                                id="productId"
+                                value={productId}
                                 placeholder="Enter the ID"
+                                onChange={handleChange}
                                 required
                             />
                         </div>
